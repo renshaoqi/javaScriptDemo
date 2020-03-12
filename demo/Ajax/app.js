9b0ea679327ac41088155ec1abca09988afa513d
renshaoqi <2303409366@qq.com>
2020-03-12 10:01:24 +0800

    ajax post请求

--- demo/Ajax/app.js
+++ demo/Ajax/app.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const path = require('path');
+const bodyParser = require('body-parser');
 const app = express();
 
 // 配置静态文件信息
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(bodyParser.urlencoded({extended: false}));
 
 // app.get('/', (req, res) => {
 //     res.send('welcome');
@@ -11,6 +14,14 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/jsonData', (req, res) => {
     res.send({ 'name': 'R先生' });
+})
+
+app.get('/get', (req, res) => {
+    res.send(req.query);
+});
+
+app.post('/post', (req, res) => {
+    res.send(req.body);
 })
 
 app.listen(7788);
