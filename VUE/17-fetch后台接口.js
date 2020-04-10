7e66bb6f49c58a64835be09381fb1bc44df89085
renshaoqi <2303409366@qq.com>
2020-04-10 22:03:03 +0800

    fetch 基本使用

---
+++ VUE/17-fetch后台接口.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const bodyParse = require('body-parser');
+
+
+const app = express();
+
+// 使用bodyParse 处理 post 请求相关配置
+app.use(bodyParse.json());
+app.use(bodyParse.urlencoded({extended: false}));
+
+
+// 跨域问题
+
+app.all('*', (req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "PUT,GET,POST,DELETE,OPTIONS");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header("Access-Control-Allow-Headers", "Content-Type");
+    next();
+})
+
+app.get('/data', (req, res) => {
+    res.send('hello');
+});
+
+app.get('/data/:id', (req, res) => {
+    res.send(req.params.id);
+});
+
+app.get('/stu', (req, res) => {
+    res.send(req.query.id);
+});
+
+app.post('/stu', (req, res) => {
+    res.send(req.body)
+})
+
+
+
+app.listen(3000);
